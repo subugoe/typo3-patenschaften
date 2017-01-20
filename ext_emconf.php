4d4bde79c0bf706bb43c319598207e09b5681def
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2017-01-20 11:56:06 +0100

    Add composer manifest

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,7 +22,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '6.2.0-7.99.99',
+            'typo3' => '6.2.0-7.6.99',
         ],
         'conflicts' => [],
         'suggests' => [],
